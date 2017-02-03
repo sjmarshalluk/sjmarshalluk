581b9362b99c02b27cce9a06ab5fc248216e071a
Steve <hello@sjmarshalluk.com>
2017-02-03 15:57:39 +0000

    so much update

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
 
-  root 'pages#index'
+  root 'pages#portfolio'
   get '/about' => 'pages#about'
+  get '/journal' => 'pages#journal'
   get '/contact' => 'pages#contact'
   get '/theround' => 'pages#theround'
   get '/smr' => 'pages#smr'
   get '/moo' => 'pages#moo'
   get '/yld' => 'pages#yld'
+  get '/containercamp' => 'pages#containercamp'
   get '/projecteo' => 'pages#projecteo'
   get '/deskbeers' => 'pages#deskbeers'
+  get '/deskbeers-design' => 'pages#deskbeers-design'
+  get '/marshallhavard' => 'pages#marshallhavard'
   get '/archive' => 'pages#archive' 
   get '/uxarchive' => 'pages#uxarchive' 
   get '/visualarchive' => 'pages#visualarchive' 
-  get '/portfolio' => 'pages#portfolio'
+  get '/work' => 'pages#portfolio'
 
 end
